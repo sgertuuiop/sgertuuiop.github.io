a558ef367ebf9e9f1c4dfae99d156499795670b4
Christian Rodríguez-Burns <christian.rodrigb@gmail.com>
2024-05-19 09:27:31 -0400

    Fix build-and-deploy workflow

--- next.config.js
+++ next.config.js
@@ -1,18 +1,23 @@
 /** @type {import('next').NextConfig} */
 
-const isGithubActions = process.env.GITHUB_ACTIONS || false
+const nextConfig = {
+  output: "export",
+};
 
-let assetPrefix = ''
-let basePath = ''
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+
+let assetPrefix = "";
+let basePath = "";
 
 if (isGithubActions) {
-  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, "");
 
-  assetPrefix = `https://${repo}/`
-  basePath = ``
+  assetPrefix = `https://${repo}/`;
+  basePath = ``;
 }
 
 module.exports = {
   assetPrefix: assetPrefix,
   basePath: basePath,
-}
+  nextConfig,
+};
